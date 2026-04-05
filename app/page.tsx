0f7c808fcb8df89364616589c2a1d22f8171cd29
aizidmoez <aizidapple@gmail.com>
2026-04-05 18:31:05 -0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-5xl font-bold">Shifting App</h1>
+    </main>
+  );
+}
